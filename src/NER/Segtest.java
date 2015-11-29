4082a5e6ca664ffe71636655f52e0691dd9002c0
chendamon <wangchendamon@gmail.com>
2015-11-29 20:24:20 +0800

    jieba_seg version

--- src/NER/Segtest.java
+++ src/NER/Segtest.java
@@ -4,8 +4,9 @@ public class Segtest
 {
 	public static void main(String[] args)
 	{
-		Ansj_seg seg = new Ansj_seg();
-		seg.Seg("【“港独”核心成员被捕 涉替诈骗内地公司案洗黑钱】“港独”组织核心成员中出羊子，其名下银行户口被揭为一宗电邮诈骗案洗黑钱。警方调查大半年后昨日上门将他拘捕，探员正在调查中出羊子的电邮纪录，倘证实诈骗电邮与他有关，则有极大机会控告他“诈骗”。");
+		//Ansj_seg seg = new Ansj_seg();
+		jieba_seg seg = new jieba_seg();
+		seg.jieba_Seg("科比是湖人球员。");
 	}
 
 }
