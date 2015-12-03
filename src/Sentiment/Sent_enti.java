19cf1c63348d763945178e7e16b75512774dd08c
chendamon <wangchendamon@gmail.com>
2015-12-03 19:15:15 +0800

    entity 跟情感词结合。

---
+++ src/Sentiment/Sent_enti.java
@@ -0,0 +1,76 @@
+package Sentiment;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/*
+ * 将实体与情感词关联
+ * 初步打算，每个情感词为1或者-1
+ * 应用之前的正负情感词表
+ * author biront
+ * date 2015/12/3
+ * 下一步的工作，实体与category tree结合，用户profile构建完成
+ * 还有去歧义的工作
+ */
+public class Sent_enti 
+{
+	HashMap<String,Integer> pos_s;
+	HashMap<String,Integer> neg_s;
+	//读情感词
+	public void readF(File f,int type) throws Exception
+	{
+		if(!f.exists())
+			throw new Exception("sentiment word file not found!");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(f),"utf-8"));
+		String line = "";
+		//int iterator = 0;
+		while((line = reader.readLine())!=null)
+		{
+			if(line.length() == 0)
+				continue;
+			else if(type == 0)
+				this.pos_s.put(line,0);
+			else
+				this.neg_s.put(line,1);
+		}
+		reader.close();
+	}
+	public void Init() throws Exception
+	{
+		this.neg_s = new HashMap<String,Integer>();
+		this.pos_s = new HashMap<String,Integer>();
+		File pfile = new File("res/正面评价词语（中文）.txt");
+		this.readF(pfile, 0);
+		pfile = new File("res/正面情感词语（中文）.txt");
+		this.readF(pfile, 0);
+		pfile = new File("res/负面评价词语（中文）.txt");
+		this.readF(pfile, 1);
+		pfile = new File("res/负面情感词语（中文）.txt");
+		this.readF(pfile, 1);
+		System.out.println("情感词加载完毕,pos: "+this.pos_s.size()+";neg: "+this.neg_s.size());
+	}
+	
+	//计算每个实体的打分
+	public int entity_SP(String entity,ArrayList<String> parse)
+	{
+		int size = parse.size();
+		StruW sw = new StruW();
+	    String sp = sw.Stru(entity, parse);
+	    if(sp == null)
+	    	return 0;
+	    else
+	    {
+	    	if(this.pos_s.containsKey(sp))
+	    		return 1;
+	    	else if(this.neg_s.containsKey(sp))
+	    		return -1;
+	    }
+		return 0;
+	}
+
+}
--- src/Sentiment/StruW.java
+++ src/Sentiment/StruW.java
@@ -1,6 +1,8 @@
 package Sentiment;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * 判断两个词是不是从属于一个分句，parse之后，用来判断情感词与实体的关系
@@ -9,19 +11,40 @@ import java.util.ArrayList;
  * 考虑语法成分吧，尝试一下
  * ccomp(喜欢-3, 科比-4)
  * advmod(詹姆斯-8, 讨厌-7)
+ * 返回与实体具有句法结构关系的word
  */
 public class StruW 
 {
-	public boolean Stru(String s,String entity,ArrayList<String> parse)
+	public String Stru(String entity,ArrayList<String> parse)
 	{
 		int size = parse.size();
 		for(int i = 0; i < size; i++)
 		{
 			String temp = parse.get(i);
-			if(temp.contains(s)&&temp.contains(entity))
-				return true;
+			if(temp.contains(entity))
+			{
+				String fi = null;
+				String f_r = "\\(.*?-";
+				Pattern p_u = Pattern.compile(f_r);
+				Matcher m = p_u.matcher(temp);
+				while(m.find())
+				{
+					fi = m.group(0).replaceAll("(", "").replaceAll("-", "");
+				}
+				f_r = ",.*?-";
+				p_u = Pattern.compile(f_r);
+				m = p_u.matcher(temp);
+				String se = null;
+				while(m.find())
+				{
+					se = m.group(0).replaceAll(",", "").replaceAll("-", "").replaceAll("\\s", "");
+				}
+				if(fi.equals(entity))
+					return se;
+				else return fi;
+			}
 		}
-		return false;
+		return null;
 	}
 
 }
