48c81d2db157346b7de569c2df9a756e9ea41ed0
chendamon <wangchendamon@gmail.com>
2015-12-07 15:18:07 +0800

    category：sentiment
    怎么计算相似度？

--- src/WikiConcept/Con_final.java
+++ src/WikiConcept/Con_final.java
@@ -1,4 +1,13 @@
 package WikiConcept;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 /*
  * 从tree里直接找到分值最大的category进行排序，取前k个
  * 作为之后的相似度计算的数据准备
@@ -7,6 +16,43 @@ package WikiConcept;
  */
 public class Con_final 
 {
-	
+	HashMap<String,Double> CP;//category point
+	public Con_final()
+	{
+		this.CP = new HashMap<String,Double>();
+	}
+	public void cal_CP(Tree_C t)
+	{
+		//计算概念的权重
+		Concept_Weight CW = new Concept_Weight();
+		Iterator it = t.getTNodes().iterator();
+		while(it.hasNext())
+		{
+			Node temp = (Node) it.next();
+			String category = temp.category_name;
+			Double sentiment = temp.getSentiment();
+			Double weight = CW.cal_Weight(category, t);
+			if(this.CP.containsKey(category))
+			{
+				double tw = this.CP.get(category);
+				this.CP.put(category, tw+sentiment*weight);
+			}
+			else this.CP.put(category, sentiment*weight);
+		}
+		//排序，方便去前K个
+		List<Map.Entry<String, Double>> fi =
+			    new ArrayList<Map.Entry<String, Double>>(CP.entrySet());
+		Collections.sort(fi, new Comparator<Map.Entry<String, Double>>() 
+		{   
+		    public int compare(Map.Entry<String, Double> o1, Map.Entry<String, Double> o2) 
+		    {      
+		        if((o2.getValue() - o1.getValue()) < 0)
+		        	return -1;
+		        else return 1;
+		        
+		        //return (o1.getKey()).toString().compareTo(o2.getKey());
+		    }
+		});
+	}
 
 }
